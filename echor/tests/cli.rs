766acfb6dce4371ca78f9bdcfc401b4d0e68b5a2
yashirook <yashiro.kentaro@gmail.com>
2024-01-28 22:25:14 +0900

    アサーションするヘルパー関数を利用するようにした

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,19 +1,33 @@
 use assert_cmd::Command;
-use predicates::prelude::*;
 use std::fs;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.assert()
+fn dies_no_args() -> TestResult {
+    Command::cargo_bin("echor")?
+        .assert()
         .failure()
         .stderr(predicates::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn run() {
-    let outfile = "tests/expected/hello.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("Hello there").assert().success().stdout(expected);
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
 }
